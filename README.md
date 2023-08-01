8fa133c019b2a201390913050a45f589d6731811
Ojas D. Sawant <ojjsaw@gmail.com>
2023-08-01 22:53:14 +0000

    intial test

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 *Gradio requires Python 3.8 or higher*
 ```
+pip install requests
+pip install beautifulsoup4
 pip install gradio
+```
+
+```
+cd files-with-gradio-api
+gradio app.py
+```
+
+```
+python port-watcher.py
 ```
--- port-watcher.py
+++ port-watcher.py
@@ -1,19 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
-def detect_gradio_app(request):
-    headers = request.headers
-    if 'gradio-app-id' in headers:
-        return True
-    payload = request.json
-    if 'app_id' in payload:
-        return True
-    ip_address = request.remote_addr
-    if ip_address == '127.0.0.1':
-        return True
-    return False
+def print_meta_properties_of_head_in_html_page_of_get_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.url)
+        print(response.headers)
+        print("-----END OF HEADER------")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        metas = soup.find_all('meta')
+        for meta in metas:
+            print(f'{meta}')
+        print("-----HTLML HEAD PROPERTIES------")
+    else:
+        print(f'Error: {response.status_code}')
 
 if __name__ == '__main__':
-    request = requests.get('http://127.0.0.1:7860')
-    print(request.headers)
-    #is_gradio_app = detect_gradio_app(request)
-    #print(is_gradio_app)
+    url = 'http://127.0.0.1:7860'
+    print_meta_properties_of_head_in_html_page_of_get_url(url)
